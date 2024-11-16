cb80cbf56c7aca108214fe540834ecf567b598ff
Diego Gomez <diegocor4@gmail.com>
2024-11-16 10:50:57 -0300

    clase entidad vineria completa

---
+++ modelos/entidadvineria.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class EntidadVineria(ABC):
+    @abstractmethod
+    def __init__(self, id: str, nombre: str):
+        self._id = id
+        self._nombre = nombre
+
+    def establecerNombre(self, nombre: str):
+        self._nombre = nombre
+
+    def obtenerId(self) -> str:
+        return self._id
+
+    def obtenerNombre(self) -> str:
+        return self._nombre
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, EntidadVineria):
+            return self._id == other._id
+        return False
