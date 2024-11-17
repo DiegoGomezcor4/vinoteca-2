663e336a87628e7efb14afb2c65d0b04676ccc8c
Diego Gomez <diegocor4@gmail.com>
2024-11-16 21:04:29 -0300

    compeltado vino

--- modelos/vino.py
+++ modelos/vino.py
@@ -7,8 +7,33 @@ class Vino(EntidadVineria):
     
     # Agregando init
     def __init__(self, id: str, nombre: str, bodega, cepas, partidas):
-        super().__init__(id, nombre)
+        super().__init__(id, nombre) #inicializa los atributos de entidad vineria
+        self._bodega = bodega  # Almacena el nombre de la bodega como string
+        self._cepas = cepas    # Lista de nombres de cepas
+        self._partidas = partidas  # Lista de cantidades por partida
 
+    # comandos
+    def establecerBodega(self, bodega):
+        self._bodega = bodega
+
+    def establecerCepas(self, cepas):
+        self._cepas = cepas
+
+    def establecerPartidas(self, partidas):
+        self._partidas = partidas
+        
+    # consultas
+    def obtenerBodega(self):
+        # Utiliza el servicio buscarBodega para obtener el objeto de tipo Bodega
+        return Vinoteca.buscarBodega(self._bodega)
+
+    def obtenerCepas(self):
+        # Utiliza el servicio buscarCepa para obtener objetos de tipo Cepa
+        return [Vinoteca.buscarCepa(cepa) for cepa in self._cepas]
+
+    def obtenerPartidas(self):
+        return self._partidas
+    
     def __repr__(self):
         return json.dumps({"nombre": self.obtenerNombre()})
 
@@ -18,7 +43,7 @@ class Vino(EntidadVineria):
             "nombre": self.obtenerNombre(),
             "bodega": self.obtenerBodega().obtenerNombre(),
             "cepas": self.__mapearCepas(),
-            "partidas": self.__partidas,
+            "partidas": self._partidas,
         }
 
     def convertirAJSONFull(self):
@@ -27,7 +52,7 @@ class Vino(EntidadVineria):
             "nombre": self.obtenerNombre(),
             "bodega": self.obtenerBodega().obtenerNombre(),
             "cepas": self.__mapearCepas(),
-            "partidas": self.__partidas,
+            "partidas": self._partidas,
         }
 
     def __mapearCepas(self):
